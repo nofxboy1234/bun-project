a9d507534a832e0be9f980626fb9d39308551c67
Dylan Palmboom <nofxboy1234@gmail.com>
2025-09-08 13:30:33 +0200

    Update base style

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import "./index.css";
 export function App() {
   return (
     <div className="app">
-      <h1>Bun + React</h1>
+      <h1>
+        <a href="https://yacine.ca/">yacine</a>
+      </h1>
+      <h2>Systems Engineer</h2>
+      <a href="">home</a>
+      <div>Hello</div>
       <APITester />
     </div>
   );
