50f1a994a3aec0e1e0f41a04c03129fe289bc16b
Dylan Palmboom <nofxboy1234@gmail.com>
2025-09-08 22:43:14 +0200

    Test css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import { APITester } from "./APITester";
 import "./index.css";
+import styles from "./styles.module.css";
 
 export function App() {
   return (
-    <div className="app">
-      <h1>
-        <a href="https://yacine.ca/">yacine</a>
-      </h1>
-      <h2>Systems Engineer</h2>
-      <a href="">home</a>
-      <div>Hello</div>
-      <APITester />
+    <div className={styles.app}>
+      <div>
+        <h1>bun-project</h1>
+        <h2>Simplify your tasks</h2>
+        <a href="">home</a>
+        <div>Hello</div>
+        <div className={`${styles.cyanSquare} ${styles.roundedCorners}`}></div>
+        <div className={`${styles.greySquare} ${styles.roundedCorners}`}></div>
+        <div className={`${styles.whiteSquare} ${styles.roundedCorners}`}></div>
+        <div className={`${styles.navySquare} ${styles.roundedCorners}`}></div>
+        <div className={`${styles.redSquare} ${styles.roundedCorners}`}></div>
+        <div
+          className={`${styles.yellowSquare} ${styles.roundedCorners}`}
+        ></div>
+        <div className={`${styles.pinkSquare} ${styles.roundedCorners}`}></div>
+      </div>
     </div>
   );
 }
