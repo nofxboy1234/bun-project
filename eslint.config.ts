5a1951b1f4667c4ce6719e337b123f7fa6fc93b2
Dylan Palmboom <nofxboy1234@gmail.com>
2025-09-08 10:23:13 +0200

    Fix eslint config and linting errors

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,8 +4,26 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+export default defineConfig(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  }
+);
