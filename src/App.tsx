40bd97e7293fc50019b7a3ac3acb35d53f87fdcd
Dylan Palmboom <nofxboy1234@gmail.com>
2025-09-09 18:35:01 +0200

    Update css

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,87 @@ export function App() {
   return (
     <div className={styles.app}>
       <div>
-        <h1 className={styles.getStarted}>Get Started with Bun</h1>
+        <h1 className={styles.getStarted}>yacine</h1>
+        <h2 className={styles.hello}>Systems Engineer</h2>
 
         <div className={styles.grid}>
+          <div className={styles.task}>
+            <div className={styles.taskContent}>
+              <div className={styles.taskTitle}>Do stuff</div>
+              <div className={styles.taskNotes}>
+                <div>Eat</div>
+                <div>Sleep</div>
+                <div>Work</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.task}>
+            <div className={styles.taskContent}>
+              <div className={styles.taskTitle}>Do stuff</div>
+              <div className={styles.taskNotes}>
+                <div>Eat</div>
+                <div>Sleep</div>
+                <div>Work</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.task}>
+            <div className={styles.taskContent}>
+              <div className={styles.taskTitle}>Do stuff</div>
+              <div className={styles.taskNotes}>
+                <div>Eat</div>
+                <div>Sleep</div>
+                <div>Work</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.task}>
+            <div className={styles.taskContent}>
+              <div className={styles.taskTitle}>Do stuff</div>
+              <div className={styles.taskNotes}>
+                <div>Eat</div>
+                <div>Sleep</div>
+                <div>Work</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.task}>
+            <div className={styles.taskContent}>
+              <div className={styles.taskTitle}>Do stuff</div>
+              <div className={styles.taskNotes}>
+                <div>Eat</div>
+                <div>Sleep</div>
+                <div>Work</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.task}>
+            <div className={styles.taskContent}>
+              <div className={styles.taskTitle}>Do stuff</div>
+              <div className={styles.taskNotes}>
+                <div>Eat</div>
+                <div>Sleep</div>
+                <div>Work</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.task}>
+            <div className={styles.taskContent}>
+              <div className={styles.taskTitle}>Do stuff</div>
+              <div className={styles.taskNotes}>
+                <div>Eat</div>
+                <div>Sleep</div>
+                <div>Work</div>
+              </div>
+            </div>
+          </div>
+
           <div className={styles.task}>
             <div className={styles.taskContent}>
               <div className={styles.taskTitle}>Do stuff</div>
