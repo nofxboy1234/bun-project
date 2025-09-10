63f526987a84c993bb3c983bf94f983033582b8f
Dylan Palmboom <nofxboy1234@gmail.com>
2025-09-10 14:54:13 +0200

    Update placeholder tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import "./index.css";
 import styles from "./styles.module.css";
-import { Task } from "./Task";
+import { Task } from "./Task2";
+import { Task as ImageTask } from "./Task";
 
 export function App() {
   const [selectedTask, setSelectedTask] = useState<number | null>(null);
 
   const tasks = [
-    { id: 0 },
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
+    { id: 0, title: "Task 1", deadline: new Date() },
+    { id: 1, title: "Task 2", deadline: new Date() },
+    { id: 2, title: "Task 3", deadline: new Date() },
+    { id: 3, title: "Task 4", deadline: new Date() },
+    { id: 4, title: "Task 5", deadline: new Date() },
+    { id: 5, title: "Task 6", deadline: new Date() },
   ];
 
   const selectTask = (index: number) => {
@@ -27,10 +28,17 @@ export function App() {
         <h1 className={styles.getStarted}>bun-project</h1>
         <h2 className={styles.hello}>Simple, Fast Tasks</h2>
 
+        <ImageTask
+          key={99}
+          id={99}
+          selectTask={selectTask}
+          selected={selectedTask === 99}
+        />
+
         {tasks.map((task) => (
           <Task
             key={task.id}
-            id={task.id}
+            task={task}
             selectTask={selectTask}
             selected={selectedTask === task.id}
           />
---
+++ src/Task2.tsx
@@ -0,0 +1,23 @@
+import styles from "./styles.module.css";
+
+export function Task({
+  selected,
+  selectTask,
+  task,
+}: {
+  selected: boolean;
+  selectTask: (index: number) => void;
+  task: { id: number; title: string; deadline: Date };
+}) {
+  return (
+    <div
+      className={`${styles.task} ${selected ? styles.selected : ""}`}
+      onClick={() => selectTask(task.id)}
+    >
+      <div className={`${styles.taskContent}`}>
+        <div className={styles.taskHeader}>{task.title}</div>
+        <div className={styles.taskFooter}>{task.deadline.toString()}</div>
+      </div>
+    </div>
+  );
+}
