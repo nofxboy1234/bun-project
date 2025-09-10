317575ab78023ad21af4c53f5536d4a83fe6187c
Dylan Palmboom <nofxboy1234@gmail.com>
2025-09-10 11:30:51 +0200

    Store selected task

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import "./index.css";
 import styles from "./styles.module.css";
 import taskImage from "./img/unnamed.png";
 
 export function App() {
+  const [selectedTask, setSelectedTask] = useState<number | null>(null);
+
+  const selectTask = (index: number) => {
+    setSelectedTask((prevSelectedTask) =>
+      prevSelectedTask === index ? null : index,
+    );
+  };
+
   return (
     <div className={styles.app}>
       <div>
         <h1 className={styles.getStarted}>yacine</h1>
         <h2 className={styles.hello}>Systems Engineer</h2>
 
-        <div className={`${styles.selected}`}>
+        <div
+          className={`${styles.task} ${
+            selectedTask === 0 ? styles.selected : ""
+          }`}
+          onClick={() => selectTask(0)}
+        >
+          <img
+            src={taskImage}
+            alt="A task to do"
+            className={`${styles.taskImage}`}
+          />
+        </div>
+
+        <div
+          className={`${styles.task} ${
+            selectedTask === 1 ? styles.selected : ""
+          }`}
+          onClick={() => selectTask(1)}
+        >
           <img
             src={taskImage}
             alt="A task to do"
