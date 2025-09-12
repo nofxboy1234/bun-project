eb8968c62e18f420fa19a76db6e8c5d65754024f
Dylan Palmboom <nofxboy1234@gmail.com>
2025-09-12 23:25:51 +0200

    Update task operation icons

--- src/Task.tsx
+++ src/Task.tsx
@@ -1,9 +1,19 @@
 import styles from "./styles.module.css";
+import deleteIcon from "@/icons/delete.svg";
+import updateIcon from "@/icons/update.svg";
 
 export function Task({
   task,
 }: {
   task: { id: number; title: string; description: string; deadline: Date };
 }) {
-  return <div className={styles.task}>{task.title}</div>;
+  return (
+    <div className={styles.task}>
+      <div>{task.title}</div>
+      <div className={styles.blocks}>
+        <img src={updateIcon} alt="Delete Task" className={styles.updateIcon} />
+        <img src={deleteIcon} alt="Delete Task" className={styles.deleteIcon} />
+      </div>
+    </div>
+  );
 }
