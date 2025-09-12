1078254920ca65ee980d4a7c11d6bfc5f7f1ff7c
Dylan Palmboom <nofxboy1234@gmail.com>
2025-09-12 10:20:11 +0200

    Reset style colours

--- src/Task.tsx
+++ src/Task.tsx
@@ -9,21 +9,5 @@ export function Task({
   selectTask: (index: number) => void;
   task: { id: number; title: string; description: string; deadline: Date };
 }) {
-  return (
-    <div
-      className={`${styles.task} ${selected ? styles.selected : ""}`}
-      onClick={() => selectTask(task.id)}
-    >
-      <div className={styles.taskContent}>
-        <div className={styles.angleRect}>{task.title}</div>
-        <div className={styles.taskHeader}>{task.description}</div>
-        <div className={styles.taskFooter}>{task.deadline.toString()}</div>
-      </div>
-
-      <div className={styles.buttons}>
-        <button onClick={(event) => event.stopPropagation()}>Update</button>
-        <button onClick={(event) => event.stopPropagation()}>Delete</button>
-      </div>
-    </div>
-  );
+  return <div className={styles.task}>{task.title}</div>;
 }
