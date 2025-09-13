0e147746d4498344d3efe931138092a317590a2d
Dylan Palmboom <nofxboy1234@gmail.com>
2025-09-13 15:54:47 +0200

    Update endpoints

--- src/index.tsx
+++ src/index.tsx
@@ -1,129 +1,40 @@
 import { serve } from "bun";
 import index from "./index.html";
-
-const tasks = [
-  {
-    id: 0,
-    title: "Task 0",
-    description: "ksdjfkjsdkf",
-    deadline: new Date(),
-  },
-  {
-    id: 1,
-    title: "Task 1",
-    description: "sdlkfjdskjkj",
-    deadline: new Date(),
-  },
-  { id: 2, title: "Task 2", description: "asdfsfsfd", deadline: new Date() },
-  {
-    id: 3,
-    title: "Task 3",
-    description: "addfdfdgfgkjkj",
-    deadline: new Date(),
-  },
-  {
-    id: 4,
-    title: "Task 4",
-    description: "sfkljjkjipwiwi",
-    deadline: new Date(),
-  },
-  {
-    id: 5,
-    title: "Task 5",
-    description: "zzzzzzzzzzjdkfjskdf",
-    deadline: new Date(),
-  },
-  {
-    id: 6,
-    title: "Task 6",
-    description: "sdklfjdskikjskjdfksdjkfjskfjdk",
-    deadline: new Date(),
-  },
-  {
-    id: 7,
-    title: "Task 7",
-    description: "the last task!",
-    deadline: new Date(),
-  },
-  {
-    id: 8,
-    title: "Task 8",
-    description: "the last task!",
-    deadline: new Date(),
-  },
-  {
-    id: 9,
-    title: "Task 9",
-    description: "the last task!",
-    deadline: new Date(),
-  },
-  {
-    id: 10,
-    title: "Task 10",
-    description: "the last task!",
-    deadline: new Date(),
-  },
-  {
-    id: 11,
-    title: "Task 11",
-    description: "the last task!",
-    deadline: new Date(),
-  },
-  {
-    id: 12,
-    title: "Task 12",
-    description: "the last task!",
-    deadline: new Date(),
-  },
-  {
-    id: 13,
-    title: "Task 13",
-    description: "the last task!",
-    deadline: new Date(),
-  },
-  {
-    id: 14,
-    title: "Task 14",
-    description: "the last task!",
-    deadline: new Date(),
-  },
-];
+import { tasks } from "./tasks";
 
 const server = serve({
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
 
-    "/api/hello": {
+    "/api/tasks": {
       async GET() {
-        return Response.json({
-          message: "Hello, world!",
-          method: "GET",
-        });
+        return Response.json(tasks);
       },
-      async PUT() {
-        return Response.json({
-          message: "Hello, world!",
-          method: "PUT",
-        });
+      async POST(req) {
+        const task = await req.json();
+        tasks.push(task);
+        return Response.json({ created: true, ...task });
       },
     },
 
-    "/api/hello/:name": async (req) => {
-      const name = req.params.name;
-      return Response.json({
-        message: `Hello, ${name}!`,
-      });
-    },
-
-    "/bun": async () => {
-      return Response.json({
-        message: "bun :)",
-      });
-    },
-
-    "/api/tasks": async (req) => {
-      return Response.json(tasks);
+    "/api/tasks/:id": {
+      async GET(req) {
+        const { id } = req.params;
+        const task = tasks.at(Number(id));
+        return Response.json(task);
+      },
+      async PATCH(req) {
+        const updatedTaskData = await req.json();
+        const task = tasks.at(updatedTaskData.id);
+        const updatedTask = { ...task, ...updatedTaskData };
+        return Response.json({ updated: true, ...updatedTask });
+      },
+      async DELETE(req) {
+        const { id } = req.params;
+        const deletedTask = tasks.splice(Number(id), 1);
+        return Response.json({ deleted: true, ...deletedTask });
+      },
     },
   },
 
---
+++ src/tasks.ts
@@ -0,0 +1,87 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Task 0",
+    description: "ksdjfkjsdkf",
+    deadline: new Date(),
+  },
+  {
+    id: 1,
+    title: "Task 1",
+    description: "sdlkfjdskjkj",
+    deadline: new Date(),
+  },
+  { id: 2, title: "Task 2", description: "asdfsfsfd", deadline: new Date() },
+  {
+    id: 3,
+    title: "Task 3",
+    description: "addfdfdgfgkjkj",
+    deadline: new Date(),
+  },
+  {
+    id: 4,
+    title: "Task 4",
+    description: "sfkljjkjipwiwi",
+    deadline: new Date(),
+  },
+  {
+    id: 5,
+    title: "Task 5",
+    description: "zzzzzzzzzzjdkfjskdf",
+    deadline: new Date(),
+  },
+  {
+    id: 6,
+    title: "Task 6",
+    description: "sdklfjdskikjskjdfksdjkfjskfjdk",
+    deadline: new Date(),
+  },
+  {
+    id: 7,
+    title: "Task 7",
+    description: "the last task!",
+    deadline: new Date(),
+  },
+  {
+    id: 8,
+    title: "Task 8",
+    description: "the last task!",
+    deadline: new Date(),
+  },
+  {
+    id: 9,
+    title: "Task 9",
+    description: "the last task!",
+    deadline: new Date(),
+  },
+  {
+    id: 10,
+    title: "Task 10",
+    description: "the last task!",
+    deadline: new Date(),
+  },
+  {
+    id: 11,
+    title: "Task 11",
+    description: "the last task!",
+    deadline: new Date(),
+  },
+  {
+    id: 12,
+    title: "Task 12",
+    description: "the last task!",
+    deadline: new Date(),
+  },
+  {
+    id: 13,
+    title: "Task 13",
+    description: "the last task!",
+    deadline: new Date(),
+  },
+  {
+    id: 14,
+    title: "Task 14",
+    description: "the last task!",
+    deadline: new Date(),
+  },
+];
