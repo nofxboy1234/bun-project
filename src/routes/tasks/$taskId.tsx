da06e1d2cdc4ee785249f04f03c0176d8e5407bd
Dylan Palmboom <nofxboy1234@gmail.com>
2025-09-29 22:10:20 +0200

    Refactor queryOptions

--- src/routes/tasks/$taskId.tsx
+++ src/routes/tasks/$taskId.tsx
@@ -1,18 +1,24 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { taskQueryOptions } from "@/taskQueryOptions";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Task } from "@/Task";
 
 export const Route = createFileRoute("/tasks/$taskId")({
   loader: async ({ context: { queryClient }, params: { taskId } }) => {
-    await queryClient.prefetchQuery(taskQueryOptions(Number(taskId)));
+    await queryClient.ensureQueryData({
+      queryKey: ["tasks", taskId],
+      queryFn: () => fetch(`/api/tasks/${taskId}`).then((r) => r.json()),
+      revalidateIfStale: true,
+    });
   },
   component: RouteComponent,
 });
 
 function RouteComponent() {
   const { taskId } = Route.useParams();
-  const { data: task } = useSuspenseQuery(taskQueryOptions(Number(taskId)));
+  const { data: task } = useSuspenseQuery({
+    queryKey: ["tasks", taskId],
+    queryFn: () => fetch(`/api/tasks/${taskId}`).then((r) => r.json()),
+  });
 
   return <Task task={task} />;
 }
