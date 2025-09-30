88adb269c864cf70384fc3d286d7562d96a59e4d
Dylan Palmboom <nofxboy1234@gmail.com>
2025-09-30 10:55:20 +0200

    Add task form inputs

--- src/TaskForm.tsx
+++ src/TaskForm.tsx
@@ -5,5 +5,43 @@ export const Route = createFileRoute("/tasks/new")({
 });
 
 export function TaskForm() {
-  return <div>TaskForm</div>;
+  return (
+    <div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log("submit");
+        }}
+      >
+        <div>
+          <label htmlFor="task-title">Title</label>
+          <input
+            type="text"
+            id="task-title"
+            name="title"
+            defaultValue="New Task"
+            placeholder="New Task"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="task-description">Description</label>
+          <input
+            type="text"
+            id="task-description"
+            name="description"
+            defaultValue="Get this done"
+            placeholder="Get this done"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="task-date">Deadline</label>
+          <input type="date" id="task-date" name="date" />
+        </div>
+
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
 }
