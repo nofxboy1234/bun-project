efe187794113c90a3cec03194cfca674a82b163c
Dylan Palmboom <nofxboy1234@gmail.com>
2025-10-10 09:19:05 +0200

    Update test tmp file

--- src/tmp.ts
+++ src/tmp.ts
@@ -1,4 +1,16 @@
 import { sql } from "bun";
 
-const result = await sql.file("./src/queries/tmp.sql");
-console.log(result);
+// const result = await sql.file("./src/queries/tmp.sql");
+// console.log(result);
+
+interface Cat {
+  id: number;
+  name: string;
+}
+
+const cat: Cat = {
+  id: 1,
+  name: "Muezza",
+};
+
+console.log(cat);
