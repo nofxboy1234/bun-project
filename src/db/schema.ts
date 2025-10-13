cc79481f78b71c8a563fe45f048489cbb3d5c24b
Dylan Palmboom <nofxboy1234@gmail.com>
2025-10-13 12:24:32 +0200

    Add index to locations table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,15 +1,19 @@
-import { pgTable, integer, varchar } from "drizzle-orm/pg-core";
+import { pgTable, integer, varchar, index } from "drizzle-orm/pg-core";
 
 export const statusesTable = pgTable("statuses", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
 });
 
-export const locationsTable = pgTable("locations", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  locationTypeId: integer().references(() => locationTypesTable.id),
-});
+export const locationsTable = pgTable(
+  "locations",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: varchar({ length: 255 }).notNull(),
+    locationTypeId: integer().references(() => locationTypesTable.id),
+  },
+  (table) => [index("location_type_id_idx").on(table.locationTypeId)],
+);
 
 export const locationTypesTable = pgTable("location_types", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
