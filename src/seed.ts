1ec6853032daf32db26e435f17ad0a1182caa6ec
Dylan Palmboom <nofxboy1234@gmail.com>
2025-10-13 23:07:57 +0200

    Update seed script

--- src/seed.ts
+++ src/seed.ts
@@ -1,9 +1,14 @@
 import { db } from "./db";
-import { seed } from "drizzle-seed";
-import { statuses } from "./db/schema";
+import { reset, seed } from "drizzle-seed";
+import { statuses } from "@/db/schema";
+import * as schema from "@/db/schema";
 
 async function main() {
-  await seed(db, { statuses });
+  await reset(db, schema);
+  await seed(db, schema, { seed: 1984 });
+
+  // await reset(db, { statuses });
+  // await seed(db, { statuses }, { seed: 1984 });
 }
 
 main();
