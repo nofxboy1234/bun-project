0a4a2210d08bf5905bdbba54ad76926998b81d74
Dylan Palmboom <nofxboy1234@gmail.com>
2025-10-13 23:20:58 +0200

    Refine seeds

--- src/seed.ts
+++ src/seed.ts
@@ -5,7 +5,15 @@ import * as schema from "@/db/schema";
 
 async function main() {
   await reset(db, schema);
-  await seed(db, schema, { seed: 1984 });
+  await seed(db, schema, { seed: 1984 }).refine((f) => ({
+    statuses: {
+      columns: {
+        name: f.valuesFromArray({
+          values: ["Dead", "Alive"],
+        }),
+      },
+    },
+  }));
 
   // await reset(db, { statuses });
   // await seed(db, { statuses }, { seed: 1984 });
