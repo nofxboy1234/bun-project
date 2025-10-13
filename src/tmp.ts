4262f1dc4cda31ca86da091f54b057b806d2dc19
Dylan Palmboom <nofxboy1234@gmail.com>
2025-10-13 21:47:38 +0200

    Add package script and update queries

--- src/tmp.ts
+++ src/tmp.ts
@@ -3,43 +3,42 @@ import { db } from "./db";
 import { eq } from "drizzle-orm";
 
 async function main() {
-  const result = await db.query.locationTypes.findMany({
+  // const o1 = { a: { b: { c: { d: { e: 5 } } } } };
+  // console.log(o1);
+  // const locationTypeCity: typeof locationTypesTable.$inferInsert = {
+  //   name: "City",
+  // };
+  // const locationTypeCountry: typeof locationTypesTable.$inferInsert = {
+  //   name: "Country",
+  // };
+  // await db.insert(locationTypesTable).values(locationTypeCity);
+  // await db.insert(locationTypesTable).values(locationTypeCountry);
+  // const locationTypesResult = await db
+  //   .select()
+  //   .from(locationTypes)
+  //   .where(eq(locationTypes.name, "Country"));
+  // const locationType = locationTypesResult[0];
+  // const location: typeof locations.$inferInsert = {
+  //   name: "South Africa",
+  //   locationTypeId: locationType!.id,
+  // };
+  // await db.insert(locations).values(location);
+  // const locationsResults = await db.select().from(locations);
+  // console.log(locationsResults);
+
+  let result;
+  result = await db.query.locationTypes.findMany({
     with: {
       locations: true,
     },
   });
   console.log(result);
 
-  // const o1 = { a: { b: { c: { d: { e: 5 } } } } };
-  // console.log(o1);
-
-  // const locationTypeCity: typeof locationTypesTable.$inferInsert = {
-  //   name: "City",
-  // };
-
-  // const locationTypeCountry: typeof locationTypesTable.$inferInsert = {
-  //   name: "Country",
-  // };
-
-  // await db.insert(locationTypesTable).values(locationTypeCity);
-  // await db.insert(locationTypesTable).values(locationTypeCountry);
-
-  // const locationTypes = await db
-  //   .select()
-  //   .from(locationTypesTable)
-  //   .where(eq(locationTypesTable.name, "City"));
-
-  // const locationType = locationTypes[0];
-
-  // const location: typeof locationsTable.$inferInsert = {
-  //   name: "Tokyo",
-  //   locationTypeId: locationType!.id,
-  // };
-
-  // await db.insert(locationsTable).values(location);
-
-  // const locations = await db.select().from(locationsTable);
-  // console.log(locations);
+  result = await db
+    .select()
+    .from(locationTypes)
+    .leftJoin(locations, eq(locationTypes.id, locations.locationTypeId));
+  console.log(result);
 }
 
 main();
