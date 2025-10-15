b82e00bdfcded90c897598396dd795d4f1695be3
Dylan Palmboom <nofxboy1234@gmail.com>
2025-10-15 08:31:43 +0200

    Add seed package script, update schema, seed schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,22 @@
 import { relations } from "drizzle-orm";
-import { pgTable, integer, varchar, index } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  integer,
+  varchar,
+  index,
+  timestamp,
+  date,
+} from "drizzle-orm/pg-core";
 
 export const statuses = pgTable("statuses", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
   name: varchar({ length: 255 }).notNull(),
 });
 
 export const locationTypes = pgTable("location_types", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
   name: varchar({ length: 255 }).notNull(),
 });
 
@@ -19,6 +28,7 @@ export const locations = pgTable(
   "locations",
   {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    created_at: timestamp().defaultNow(),
     name: varchar({ length: 255 }).notNull(),
     locationTypeId: integer()
       .references(() => locationTypes.id, {
@@ -35,3 +45,77 @@ export const locationsRelations = relations(locations, ({ one }) => ({
     references: [locationTypes.id],
   }),
 }));
+
+export const characterAliases = pgTable("character_aliases", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
+  name: varchar({ length: 255 }).notNull(),
+});
+
+export const genders = pgTable("genders", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
+  name: varchar({ length: 255 }).notNull(),
+});
+
+export const speciesAliases = pgTable("species_aliases", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
+  name: varchar({ length: 255 }).notNull(),
+});
+
+export const species = pgTable("species", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
+  name: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 255 }).notNull(),
+});
+
+export const maps = pgTable("maps", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
+  imageFilePath: varchar({ length: 255 }).notNull(),
+});
+
+export const relatives = pgTable("relatives", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
+  type: varchar({ length: 255 }).notNull(),
+});
+
+export const contracts = pgTable("contracts", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
+  terms: varchar({ length: 255 }).notNull(),
+});
+
+export const characters = pgTable("characters", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer(),
+  height: integer(),
+  birthday: date(),
+});
+
+export const characterAffiliations = pgTable("character_affiliations", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
+});
+
+export const affiliations = pgTable("affiliations", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
+  name: varchar({ length: 255 }).notNull(),
+});
+
+export const character_occupations = pgTable("character_occupations", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
+});
+
+export const occupations = pgTable("occupations", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().defaultNow(),
+  name: varchar({ length: 255 }).notNull(),
+});
