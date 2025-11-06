30aadc7a9bc591d72ab9a7263485c6113e434bdd
Dylan Palmboom <nofxboy1234@gmail.com>
2025-11-06 13:30:55 +0200

    Update seed2 script

--- seed2.ts
+++ seed2.ts
@@ -1,13 +1,21 @@
 import { sql } from "bun";
 import {
   statuses,
-  // characters,
-  // genders,
-  // locationTypes,
-  // locations,
-  // species,
+  characters,
+  genders,
+  locationTypes,
+  locations,
+  species,
 } from "./seedData";
-import type { StatusSelectModel, TableData } from "@/types";
+import type {
+  TableData,
+  StatusSelectModel,
+  GenderSelectModel,
+  SpeciesSelectModel,
+  LocationSelectModel,
+  CharacterSelectModel,
+  LocationTypeSelectModel,
+} from "@/types";
 
 const camelToSnake = (str: string) =>
   str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
@@ -34,11 +42,59 @@ const seed = async (table: TableData) => {
   const models = table.data;
   const cleanModels = models.map(cleanModel);
 
-  const records = await sql<StatusSelectModel[]>`
-          INSERT INTO ${sql(table.table)} ${sql(cleanModels)}
-          ON CONFLICT DO NOTHING
-          RETURNING *
-          `;
+  let records: Array<
+    | StatusSelectModel
+    | CharacterSelectModel
+    | GenderSelectModel
+    | LocationSelectModel
+    | LocationTypeSelectModel
+    | SpeciesSelectModel
+  >;
+
+  switch (table.table) {
+    case "statuses":
+      records = await sql<StatusSelectModel[]>`
+              INSERT INTO ${sql(table.table)} ${sql(cleanModels)}
+              ON CONFLICT DO NOTHING
+              RETURNING *
+              `;
+      break;
+    case "characters":
+      records = await sql<CharacterSelectModel[]>`
+              INSERT INTO ${sql(table.table)} ${sql(cleanModels)}
+              ON CONFLICT DO NOTHING
+              RETURNING *
+              `;
+      break;
+    case "genders":
+      records = await sql<GenderSelectModel[]>`
+              INSERT INTO ${sql(table.table)} ${sql(cleanModels)}
+              ON CONFLICT DO NOTHING
+              RETURNING *
+              `;
+      break;
+    case "location_types":
+      records = await sql<LocationTypeSelectModel[]>`
+              INSERT INTO ${sql(table.table)} ${sql(cleanModels)}
+              ON CONFLICT DO NOTHING
+              RETURNING *
+              `;
+      break;
+    case "locations":
+      records = await sql<LocationSelectModel[]>`
+              INSERT INTO ${sql(table.table)} ${sql(cleanModels)}
+              ON CONFLICT DO NOTHING
+              RETURNING *
+              `;
+      break;
+    case "species":
+      records = await sql<SpeciesSelectModel[]>`
+              INSERT INTO ${sql(table.table)} ${sql(cleanModels)}
+              ON CONFLICT DO NOTHING
+              RETURNING *
+              `;
+      break;
+  }
 
   models.forEach((model) => {
     const idRecord = records.map(cleanRecord).find((record) =>
@@ -51,15 +107,25 @@ const seed = async (table: TableData) => {
         ),
     );
 
+    console.log(model);
+    console.log(idRecord);
     model.id = idRecord.id;
   });
 };
 
-const main = () => {
-  // [statuses, genders, species, locationTypes, locations, characters].forEach(
-  //   (table) => seed(table),
-  // );
-  [statuses].forEach((table) => seed(table));
+const main = async () => {
+  const tablesToSeed = [
+    statuses,
+    genders,
+    species,
+    locationTypes,
+    locations,
+    characters,
+  ];
+
+  for (const table of tablesToSeed) {
+    await seed(table);
+  }
 };
 
 main();
