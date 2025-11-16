3f8f975c954ac716cf9019cc2aa54060f64e4bea
Dylan Palmboom <nofxboy1234@gmail.com>
2025-11-16 22:20:58 +0200

    Update eden treaty api call

--- src/client.ts
+++ src/client.ts
@@ -3,5 +3,5 @@ import type { App } from "@/index";
 
 const client = treaty<App>("localhost:3000");
 
-const { data: index } = await client.v1.get();
+const { data: index } = await client.api.v1.tasks.get();
 console.log(index);
