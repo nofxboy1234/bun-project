48b134e848db6aef6a7f2f79f006ad22fc3b74df
Dylan Palmboom <nofxboy1234@gmail.com>
2025-11-21 12:05:01 +0200

    clean up unused files, rename files

--- src/client.ts
+++
@@ -1,7 +0,0 @@
-import { treaty } from "@elysiajs/eden";
-import type { Api } from "@/index";
-
-const client = treaty<Api>("localhost:3000");
-
-const { data: index } = await client.api.v1.tasks.get();
-console.log(index);
