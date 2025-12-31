abf551e042ed5411e7839fcce12af335772e2103
Dylan Palmboom <nofxboy1234@gmail.com>
2025-12-31 16:48:18 +0200

    Update typebox and zod testing code

--- src/frontendTypes/types.ts
+++ src/frontendTypes/types.ts
@@ -1,7 +1,15 @@
 import { createInsertSchema } from "drizzle-zod";
+import { createInsertSchema as createTypeboxInsertSchema } from "drizzle-typebox";
+import { Value } from "@sinclair/typebox/value";
+
 import * as schema from "@/db/schema";
 
-const affiliationInsertSchema = createInsertSchema(schema.affiliations);
-
 const affiliation = { name: "Public Safety" };
+const affiliationInsertSchema = createInsertSchema(schema.affiliations);
 const parsed = affiliationInsertSchema.parse(affiliation);
+
+const affiliation2 = { name: "Public Safety" };
+const affiliationTypeboxInsertSchema = createTypeboxInsertSchema(
+  schema.affiliations,
+);
+const parsed2 = Value.Parse(affiliationTypeboxInsertSchema, affiliation2);
