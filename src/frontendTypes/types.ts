9efa25d2fcf9a55e0c374c296cb4a4f14468cb54
Dylan Palmboom <nofxboy1234@gmail.com>
2026-01-02 17:18:43 +0200

    Test typebox type coercion

--- src/frontendTypes/types.ts
+++ src/frontendTypes/types.ts
@@ -3,6 +3,7 @@ import { createInsertSchema as createTypeboxInsertSchema } from "drizzle-typebox
 import { Value } from "@sinclair/typebox/value";
 
 import * as schema from "@/db/schema";
+import { Type } from "@sinclair/typebox";
 
 const affiliation = { name: "Public Safety" };
 const affiliationInsertSchema = createInsertSchema(schema.affiliations);
@@ -13,3 +14,23 @@ const affiliationTypeboxInsertSchema = createTypeboxInsertSchema(
   schema.affiliations,
 );
 const parsed2 = Value.Parse(affiliationTypeboxInsertSchema, affiliation2);
+
+const character = {
+  name: "dylan",
+  speciesId: 2,
+  genderId: 3,
+  birthplaceId: 999,
+  statusId: "1",
+};
+const characterTypeboxInsertSchema = createTypeboxInsertSchema(
+  schema.characters,
+  {
+    statusId: Type.Integer(),
+  },
+);
+try {
+  const parsed3 = Value.Parse(characterTypeboxInsertSchema, character);
+  console.log("Parsed character:", parsed3);
+} catch (e) {
+  console.error("Validation failed:", e);
+}
