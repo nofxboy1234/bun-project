1730481f7afd78d095cba9de1a2252042b6465db
Dylan Palmboom <nofxboy1234@gmail.com>
2026-01-19 16:15:26 +0200

    Update api

--- src/routes/api.v1.$.ts
+++ src/routes/api.v1.$.ts
@@ -28,109 +28,108 @@ export const app = new Elysia({
   .error({
     DrizzleQueryError,
   })
-  .onError(({ code, error, set }) => {
-    switch (code) {
-      case "DrizzleQueryError":
-        set.status = 500;
-
-        return {
-          name: error.name,
-          query: error.query,
-          message: error.cause?.message,
-        };
-    }
+  .model({
+    drizzleError: drizzle.DrizzleQueryError,
   })
-  .get("/location-types", async () => await getLocationTypes(), {
-    response: { 200: t.Array(select.locationType) },
-  })
-  .get("/locations", async () => await getLocations(), {
-    response: { 200: t.Array(select.location) },
-  })
-  .post(
-    "/locations",
-    async ({ body }) => {
-      console.log("### post /locations handler");
-      return await insertLocation(body);
-    },
+  .guard(
     {
-      body: insert.location,
       response: {
-        500: drizzle.DrizzleQueryError,
-        200: select.location,
+        500: "drizzleError",
+      },
+      error({ code, error, status }) {
+        switch (code) {
+          case "DrizzleQueryError":
+            const err = error as Readonly<DrizzleQueryError>;
+
+            return status(500, {
+              name: err.name,
+              query: err.query,
+              message: err.cause?.message,
+            });
+        }
       },
     },
-  )
-  .get(
-    "/locations/:id",
-    async ({ params: { id }, set }) => {
-      const location = await getLocation(id);
+    (app) =>
+      app
+        .get("/location-types", async () => await getLocationTypes(), {
+          response: { 200: t.Array(select.locationType) },
+        })
+        .get("/locations", async () => await getLocations(), {
+          response: { 200: t.Array(select.location) },
+        })
+        .post(
+          "/locations",
+          async ({ body }) => {
+            console.log("### post /locations handler");
+            return await insertLocation(body);
+          },
+          {
+            body: insert.location,
+            response: {
+              200: select.location,
+            },
+          },
+        )
+        .get(
+          "/locations/:id",
+          async ({ params: { id }, set }) => {
+            const location = await getLocation(id);
 
-      if (!location) {
-        console.log("get /locations/:id - not found");
+            if (!location) {
+              console.log("get /locations/:id - not found");
 
-        set.status = 404;
-        return { success: false, error: "Not Found" };
-      }
+              set.status = 404;
+              return { success: false, error: "Not Found" };
+            }
 
-      return location;
-    },
-    {
-      params: t.Object({
-        id: t.Number(),
-      }),
-      response: {
-        200: select.location,
-        404: t.Object({
-          success: t.Boolean(),
-          error: t.String(),
-        }),
-      },
-    },
-  )
-  .patch(
-    "/locations/:id",
-    async ({ params: { id }, body, set }) => {
-      const location = await updateLocation(id, body);
+            return location;
+          },
+          {
+            params: t.Object({
+              id: t.Number(),
+            }),
+            response: {
+              200: select.location,
+              404: t.Object({
+                success: t.Boolean(),
+                error: t.String(),
+              }),
+            },
+          },
+        )
+        .patch(
+          "/locations/:id",
+          async ({ params: { id }, body }) => {
+            const location = await updateLocation(id, body);
 
-      if (!location) {
-        set.status = 500;
-        return { success: false, error: "Failed to update location" };
-      }
+            return location;
+          },
+          {
+            params: t.Object({
+              id: t.Number(),
+            }),
+            body: update.location,
+            response: {
+              200: select.location,
+            },
+          },
+        )
+        .delete(
+          "/locations/:id",
+          async ({ params: { id } }) => {
+            const location = await deleteLocation(id);
 
-      return location;
-    },
-    {
-      params: t.Object({
-        id: t.Number(),
-      }),
-      body: update.location,
-      response: {
-        200: select.location,
-        500: t.Object({ success: t.Boolean(), error: t.String() }),
-      },
-    },
-  )
-  .delete(
-    "/locations/:id",
-    async ({ params: { id }, set }) => {
-      const location = await deleteLocation(id);
-
-      if (!location) {
-        set.status = 500;
-        return { success: false, error: "Failed to delete location" };
-      }
-
-      return location;
-    },
-    {
-      params: t.Object({
-        id: t.Number(),
-      }),
-      response: {
-        200: select.location,
-        500: t.Object({ success: t.Boolean(), error: t.String() }),
-      },
-    },
+            return location;
+          },
+          {
+            params: t.Object({
+              id: t.Number(),
+            }),
+            response: {
+              200: select.location,
+            },
+          },
+        ),
   );
 
 const handle = ({ request }: { request: Request }) => {
