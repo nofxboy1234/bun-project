58e355909a60ee731814e636c66569bd0b4ee293
Dylan Palmboom <nofxboy1234@gmail.com>
2026-01-21 14:24:38 +0200

    Update react component prop type

--- src/components/LocationPreview.tsx
+++ src/components/LocationPreview.tsx
@@ -5,7 +5,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Link, useNavigate } from "@tanstack/react-router";
 import { api } from "../routes/api.v1.$";
 import type { ValidationError } from "elysia";
-import { SelectLocation } from "@/types/validation/zod/select";
+import { Location } from "@/modules/location/model";
 
 const deleteLocation = async (id: number) => {
   const { data, error } = await api().v1.locations({ id }).delete();
@@ -15,7 +15,7 @@ const deleteLocation = async (id: number) => {
   return data;
 };
 
-export function LocationPreview({ location }: { location: SelectLocation }) {
+export function LocationPreview({ location }: { location: Location.select }) {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
 
